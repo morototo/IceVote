0b0c571450d2c09dfedd1d1165cbc98e5a4fb06d
Kyasi Moro <toto7249@gmail.com>
2020-02-13 22:02:10 +0900

    SignInページの作成

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import SignIn from './pages/sign_in/sign_in'
+
+const isAuthenticated = true
+
+const App = () => (
+  <div className="App">
+  <p>{`isAuthenticated: ${isAuthenticated}`}</p>
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/signin">SingIn</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Switch>
+        <Route exact path="/signin" render={props => isAuthenticated
+          ? (<Redirect to="/"/>)
+          : (<SignIn {...props}/>)
+        }/>
+      </Switch>
     </div>
-  );
-}
+  </Router>
+</div>
+)
 
-export default App;
+
+export default App
